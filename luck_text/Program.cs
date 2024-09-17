5965ce36e3d8c3849222e1a77022d1aec7a89b61
teo erfass josefsson <teo.erfassjosefsson@elev.ga.ntig.se>
2024-09-17 12:26:16 +0200

    fgbgtrsrstg

---
+++ luck_text/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("vad ditt favorit djur säger om dig?");
+string djur = Console.ReadLine();
+
+if (djur == "hund")
+{
+    Console.WriteLine("du är en människa");
+};
+if (djur == "katt")
+{
+    Console.WriteLine("du går till kattcafe");
+};
+if (djur == "kanin")
+{
+    Console.WriteLine("du gillar att käka grönsaker");
+};
+if (djur == "varg")
+{
+    Console.WriteLine("ojojoj");
+};
+if (djur == "rotta")
+{
+    Console.WriteLine("bara varför?");
+}
+
+
+
+Console.ReadLine();
